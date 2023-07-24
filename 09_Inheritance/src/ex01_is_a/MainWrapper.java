4e0940a8e2138990ffbbbbc89708e99b05d555eb
kdh23232 <kdh23232@naver.com>
2023-07-24 09:29:37 +0900

    20230724

---
+++ 09_Inheritance/src/ex01_is_a/MainWrapper.java
@@ -0,0 +1,18 @@
+package ex01_is_a;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    // Student 객체 선언 & 생성
+    Student s = new Student();
+    
+    // Student 객체 메소드 확인
+    s.eat();
+    s.sleep();
+    s.study();
+    
+
+  }
+
+}
