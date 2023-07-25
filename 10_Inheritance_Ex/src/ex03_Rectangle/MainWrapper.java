07944480c4c2eb2aeb1fdc7e8d5c6b7a0934eb02
kdh23232 <kdh23232@naver.com>
2023-07-25 11:23:04 +0900

    20230725

---
+++ 10_Inheritance_Ex/src/ex03_Rectangle/MainWrapper.java
@@ -0,0 +1,15 @@
+package ex03_Rectangle;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    // 정사각형 만들기
+    Square s1 = new Square();
+    s1.setWidth(5);
+    s1.setHeight(5);
+    s1.info();
+
+  }
+
+}
---
+++ 10_Inheritance_Ex/src/ex03_Rectangle/Square.java
@@ -0,0 +1,7 @@
+package ex03_Rectangle;
+
+public class Square extends Rectangle {
+
+  
+  
+}
